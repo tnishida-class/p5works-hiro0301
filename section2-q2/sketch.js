ebd94602888337dd88deccd32fca46f362de02bf
hiro0301 <hiro.gyakutenda.0301@icloud.com>
2020-11-17 12:12:00 +0900

    section q-2 submission test

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -5,7 +5,13 @@ function setup() {
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
-      // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+      rect(size * i, size * j, size ,size)
+      if((itj) %2 ==0){
+        fill(0);
+      }
+      else{
+        fill(150);
+      }
     }
   }
 }
