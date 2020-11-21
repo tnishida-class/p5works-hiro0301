1807eea95d76d070a23af9ddc421d6622dd4071b
hiro0301 <hiro.gyakutenda.0301@icloud.com>
2020-11-21 20:04:03 +0900

    Answer section2-q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -5,13 +5,31 @@ function setup() {
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
-      rect(size * i, size * j, size ,size)
-      if((itj) %2 ==0){
-        fill(0);
+      if((i+j) %2 ==0){
+        fill(255);
       }
       else{
-        fill(150);
+        fill(100);
+      }
+      rect(size * i, size * j, size ,size);
+    }
+  for(let n = 0; n < 8; n++){
+    for(let m = 0; m < 8; m++){
+      let r=20;
+      if((n+m)  %2 ==1 && m<3){
+        fill(255,0,0);
+      }
+      else if((n+m)  %2 ==1 && m>=3 && m<5){
+        fill(100);
+}
+      else if((n+m)  %2 ==1 && m>=5){
+        fill(0,0,255);
+      }
+      else{
+        fill(255);
+      }
+        ellipse(12.5+size*n, 12.5+size*m, r, r);
+    }
       }
     }
   }
-}
