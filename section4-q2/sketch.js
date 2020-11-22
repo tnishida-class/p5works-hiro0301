2cf99f5ac27a654528ad242562260ecee3c5a5d6
hiro0301 <hiro.gyakutenda.0301@icloud.com>
2020-11-23 07:54:57 +0900

    Answer section4-q2

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -5,26 +5,29 @@ function setup(){
   createCanvas(windowWidth, windowHeight);
   x = width / 2;
   y = height / 2;
-  vx = 8;
-  vy = 8;
+  vx = 4;
+  vy = 4;
+  i=0;
 }
 
 function draw(){
   background(160, 192, 255);
-  // BLANK[2] (hint: 作った star 関数を使います)
-
-  // 端の処理パターン (1) 反対側から出てくる
+  noStroke();
+  fill(255, 204, 100);
+  star(x, y, 10, i);
   if(x > width){ x = 0; }
   else if(x < 0){ x = width; }
   if(y > height){ y = 0; }
   if(y < 0){ y = height; }
+  x+=vx;
+  y+=vy;
+  i+=10
 }
 
 function star(cx, cy, r, angle){
   beginShape();
   for(var i = 0; i < 20; i++){
-    var theta = TWO_PI * i * 2 / 5 - HALF_PI;
-    // BLANK[1] (hint: angle 分だけ星を回転させるには？)
+    var theta = TWO_PI * i * 2 / 5 - HALF_PI + angle;
     var x = cx + cos(theta) * r;
     var y = cy + sin(theta) * r;
     vertex(x,y);
